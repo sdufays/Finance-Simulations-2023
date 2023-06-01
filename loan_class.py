b5a433195f05bc0240e3a3317895c58d6b07a352
Jennifer Chiou <Jchiou@lnrproperty.com>
2023-06-01 20:35:09 +0000

    Updated loan_class.py

--- loan_class.py
+++ loan_class.py
@@ -4,22 +4,27 @@ import random
 class Loan:
     def __init__(self, loan_id, collateral_balance, margin, index_floor, remaining_loan_term, extension_period, open_prepayment_period):
         self.__loan_id = loan_id
-        self.__collateral_balance = collateral_balance
+        self.__loan_balance = collateral_balance
         self.__margin = margin
         self.__index_floor = index_floor
         self.__remaining_loan_term = remaining_loan_term
         self.__extension_period = extension_period
         self.__open_prepayment_period = open_prepayment_period
-
+        self.__term_length = 0
+  
     def get_loan_id(self):
         return self.__loan_id
     
     def get_prev_id(self):
         return self.__prev_id
 
-    def get_collateral_balance(self):
-        return self.__collateral_balance
+    def get_loan_balance(self):
+        return self.__loan_balance
 
+    # need all the equations for investors to pay off their loans each month 
+    def update_loan_balance(self, month, etc):
+        # add equation for loan getting smaller and smaller each month
+  
     def get_margin(self):
         return self.__margin
 
@@ -35,13 +40,8 @@ class Loan:
     def get_open_prepayment_period(self):
         return self.__open_prepayment_period
 
-    def get_term(self):
-        # make this align with ratios
-        term_type = random.choice(['initial', 'extended', 'prepaid'])
-        if term_type == "initial":
-            return(self.get_remaining_loan_term())
-        elif term_type == "extended":
-            return(self.get_remaining_loan_term() + self.get_extension_period())
-        else:
-            return(self.get_open_prepayment_period())
-        
+    def set_term_length(self, term):
+        self.__term_length = term
+
+    def get_term_length(self):
+        return self.__term_length
