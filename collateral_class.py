609bff12abca4c82a9c6e24a6f8956a0c65bb019
Jennifer Chiou <Jchiou@lnrproperty.com>
2023-06-01 15:52:59 +0000

    fixed error, made it easier to add loan

--- collateral_class.py
+++ collateral_class.py
@@ -1,10 +1,11 @@
 from loan_class import Loan
 
-def CollateralPortfolio(Loan):
+class CollateralPortfolio(Loan):
     def __init__(self):
         self.__portfolio = []
 
-    def add_loan(self, loan):
+    def add_loan(self, loan_id, collateral_balance, margin, index_floor, remaining_loan_term, extension_period, open_prepayment_period):
+        loan = Loan(loan_id, collateral_balance, margin, index_floor, remaining_loan_term, extension_period, open_prepayment_period)
         self.__portfolio.append(loan)
 
     def get_collateral_sum(collateral_portfolio):
