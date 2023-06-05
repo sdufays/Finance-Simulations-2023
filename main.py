8e2470ba03b811bc539fb31fe0eab7d10f5bc14b
Sarah <sdufays@lnrproperty.com>
2023-06-05 17:01:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -108,8 +108,6 @@ if __name__ == "__main__":
         # paying off loans
         if principal_pay != 0: 
            loan_portfolio.remove_loan(loan)
-           # calculate interest paid based on tranche's spread + sofr and the balance of the tranche at that time
-           # principal payment = after the reinvestment period, the balance of whatever was paid off (goes back to the bondholders)
            #  clo cash flows total = total discount amount (0.75% x balance of each tranch) + total upfront costs - sum of all original balances of tranches that was solod (period 1 )
            # interest payment + principal payment for each trNCHD 
 
@@ -118,7 +116,7 @@ if __name__ == "__main__":
               loan_portfolio.add_new_loan(beginning_bal)
            else:
                clo.get_tranches()[0].subtract_size(beginning_bal)
-               prin
+               principal_tranche = beginning_bal
 
         # terminate 
         if clo.get_tranches()[0].get_size() <= threshold:
