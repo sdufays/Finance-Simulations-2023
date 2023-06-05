7918eef807e945836f6e67c53b8f61b433e05fc5
Elizabeth McGuire <Emcguire@lnrproperty.com>
2023-06-05 14:40:49 -0400

    updated main for reading excel

--- clo_test.py
+++ clo_test.py
@@ -1,6 +1,13 @@
 from clo_class import CLO
 import pandas as pd
 
+def get_date_array(date):
+    if date[2] % 4 == 0:
+      return [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    else: 
+      return [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
 if __name__ == "__main__":
     # ------------------------ GENERAL INFO ------------------------ #
     base = [.33, .33, .34]
@@ -8,12 +15,13 @@ if __name__ == "__main__":
     upside = [.40, .35, .25]
 
     # read excel file for Other Specifications
-    df_os = pd.read_excel("CLO_Input.xlsm", sheet_name = "Other Specifications")
+    df_os = pd.read_excel("CLO_Input.xlsm", sheet_name = "Other Specifications", header=None)
 
     # assume they're giving us a date at the end of the month
     # they don't start at the start, they start when the first payment is made
-    first_payment_date = df_os.iloc[1, 2]
-    date = first_payment_date.split("/") # ["MM", "DD", "YYYY"]
+    first_payment_date = df_os.iloc[0, 1]
+    date_str = first_payment_date.strftime("%m-%d-%Y")
+    date = date_str.split("-") # ["MM", "DD", "YYYY"]
     date = list(map(int, date)) # [MM, DD, YYYY]
     # starting payment month
     starting_month = date[0]
@@ -23,17 +31,17 @@ if __name__ == "__main__":
 
     # --------------------------- UPFRONT COSTS --------------------------- #
 
-    df_uc = pd.read_excel("CLO_Input.xlsm", sheet_name = "Upfront Costs")
-    legal = df_uc.iloc[1, 0]
+    df_uc = pd.read_excel("CLO_Input.xlsm", sheet_name = "Upfront Costs", header=None)
+    legal = df_uc.iloc[0, 1]
     accounting = df_uc.iloc[1, 1]
-    trustee = df_uc.iloc[1, 2]
-    printing = df_uc.iloc[1, 3]
-    RA_site = df_uc.iloc[1, 4]
-    modeling = df_uc.iloc[1, 5]
-    misc = df_uc.iloc[1, 6]
+    trustee = df_uc.iloc[2, 1]
+    printing = df_uc.iloc[3, 1]
+    RA_site = df_uc.iloc[4, 1]
+    modeling = df_uc.iloc[5, 1]
+    misc = df_uc.iloc[6, 1]
 
     # ------------------------ INITIALIZE OBJECTS ------------------------ #
-    ramp_up = df_os.iloc[1, 0]
+    ramp_up = df_os.iloc[0, 1]
     clo = CLO(ramp_up)
 
     # read excel file for capital stack
